2d1233ab636e3b5f1930f37bae7d81b30e0a8f41
Christian Taylor <christian.taylor@machinology.com>
2024-05-05 07:16:21 +0000

    update restore script

--- restore.sh
+++ restore.sh
@@ -1,2 +1,5 @@
-cp -r ./conf /etc/nixos 
-cp -r ./home-manager ~/.config/home-manager 
+sudo cp -r ./conf/* /etc/nixos/
+sudo nixos-rebuild switch
+
+cp -r ./home-manager/* ~/.config/home-manager/
+home-manager switch
