b42e1eecce5a824950080afccd8106c1544372a3
Christian Taylor <christianjtaylor@sky.com>
2024-06-16 20:33:31 +0100

    a better lua init?

--- home-manager/apps/nixvim/nixvim-extraConfigLua.nix
+++ home-manager/apps/nixvim/nixvim-extraConfigLua.nix
@@ -3,8 +3,20 @@
   programs.nixvim = {
 
     extraConfigLua = ''
-     local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-     capabilities.textDocument.completion.completionItem.snippetSupport = true
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      },
+    }
+    require("cmp_nvim_lsp").default_capabilities(capabilities) -- for nvim-cmp
     '';
 
   };
