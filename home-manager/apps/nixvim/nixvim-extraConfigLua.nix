7ab81240a78eaaa78f4d366894d197b1b8cfff68
Christian Taylor <christianjtaylor@sky.com>
2024-07-06 11:13:22 +0100

    tidy up is all

--- home-manager/apps/nixvim/nixvim-extraConfigLua.nix
+++ home-manager/apps/nixvim/nixvim-extraConfigLua.nix
@@ -3,23 +3,22 @@
   programs.nixvim = {
 
     extraConfigLua = ''
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
-    }
-    capabilities.textDocument.completion.completionItem.resolveSupport = {
-      properties = {
-        "documentation",
-        "detail",
-        "additionalTextEdits",
-      },
-    }
-    require("cmp_nvim_lsp").default_capabilities(capabilities) -- for nvim-cmp
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+          "documentation",
+          "detail",
+          "additionalTextEdits",
+        },
+      }
+      require("cmp_nvim_lsp").default_capabilities(capabilities) -- for nvim-cmp
     '';
 
   };
-
 }
 
