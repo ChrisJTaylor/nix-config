bd507a8b7088a0a248da2782b797a82f8cde71ba
Christian Taylor <christianjtaylor@sky.com>
2024-09-20 19:57:08 +0100

    more apple thingies

--- nixos/system/common-darwin.nix
+++ nixos/system/common-darwin.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   nixpkgs.config.allowUnfree = true;
+  
   security.pam.enableSudoTouchIdAuth = true;
+  security.pki.installCACerts = true;
+
+  fonts.packages = [
+    pkgs.font-awesome_5
+  ];
 }
