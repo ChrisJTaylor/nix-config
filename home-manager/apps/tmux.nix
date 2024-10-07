eaac507a2b31e1f9725b0e7b307eb007b8edcbe6
Christian Taylor <christianjtaylor@sky.com>
2024-10-07 07:06:00 +0000

    tmux tweaks

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -13,6 +13,7 @@
       pkgs.tmuxPlugins.tmux-fzf
     ];
     extraConfig = ''
+    set -g @continuum-boot-options 'on' 
     set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
     run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
     '';
