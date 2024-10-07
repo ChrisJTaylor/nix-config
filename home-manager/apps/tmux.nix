489404e3e596049614174112fab9573573f6dbea
Christian Taylor <christianjtaylor@sky.com>
2024-10-07 06:42:04 +0000

    tmux tweaks

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -4,15 +4,15 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
+    newSession = true;
     plugins = [
-      pkgs.tmuxPlugins.weather
       pkgs.tmuxPlugins.continuum
       pkgs.tmuxPlugins.fzf-tmux-url
       pkgs.tmuxPlugins.catppuccin
       pkgs.tmuxPlugins.session-wizard
+      pkgs.tmuxPlugins.tmux-fzf
     ];
     extraConfig = ''
-    set-option -g status-right '#{weather}'
     set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
     run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
     '';
