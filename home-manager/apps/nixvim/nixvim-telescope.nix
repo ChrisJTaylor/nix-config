87b7259e67811a0f2819c158c4f8aa2e3ac64a5e
Christian Taylor <christianjtaylor@sky.com>
2024-11-01 03:01:28 +0000

    add fzf and undo to telescope

--- home-manager/apps/nixvim/nixvim-telescope.nix
+++ home-manager/apps/nixvim/nixvim-telescope.nix
@@ -25,6 +25,47 @@
 	  ];
 	};
       };
+      extensions = {
+        fzf-native = {
+          enable = true;
+          settings = {
+            case_mode = "ignore_case";
+            fuzzy = true;
+            override_file_sorter = true;
+            override_generic_sorter = true;
+          };
+        };
+        undo = {
+          enable = true;
+          settings = {
+            entry_format = "state #$ID";
+            mappings = {
+              i = {
+                "<c-cr>" = "require('telescope-undo.actions').restore";
+                "<cr>" = "require('telescope-undo.actions').yank_additions";
+                "<s-cr>" = "require('telescope-undo.actions').yank_deletions";
+              };
+              n = {
+                Y = "require('telescope-undo.actions').yank_deletions";
+                u = "require('telescope-undo.actions').restore";
+                y = "require('telescope-undo.actions').yank_additions";
+              };
+            };
+            side_by_side = true;
+            time_format = "!%Y-%m-%dT%TZ";
+            use_custom_command = [
+              "bash"
+              "-c"
+              "echo '$DIFF' | delta"
+              ];
+              use_delta = true;
+              vim_diff_opts = {
+              ctxlen = 8;
+              };
+              };
+        };
+      };
+
     };
 
   };
