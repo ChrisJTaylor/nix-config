6e25556945ad57bd1729a16c0d8d7e0fdbee2e48
Christian Taylor <christianjtaylor@sky.com>
2024-11-16 16:22:57 +0000

    start creating network for containers

--- nixos/services/teamcity.nix
+++ nixos/services/teamcity.nix
@@ -32,10 +32,28 @@ let version = {
 
   };
 
+  networking = {
+    bridges.br0.interfaces = [ "enp7s0f0" ];
+
+    useDHCP = false;
+    interfaces."br0".useDHCP = true;
+
+    interfaces."br0".ipv4.addresses = [{
+      address = "192.168.100.3";
+      prefixLength = 24;
+    }];
+    defaultGateway = "192.168.100.1";
+    nameservers = [ "192.168.100.1" ];
+  };
+
   containers = {
 
     agent01 = {
       autoStart = true;
+      hostBridge = "br0";
+      privateNetwork = true;
+      localAddress = "192.168.100.5/24";
+    
       config = { config, pkgs, ...}: 
       {
         system.stateVersion = "24.05";
