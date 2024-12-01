d5a5d0456ba705ad7a2f1cd517e17683afa018e2
Christian Taylor <christianjtaylor@sky.com>
2024-12-01 08:41:09 +0000

    add gh cli

--- home-manager/apps/apps.nix
+++ home-manager/apps/apps.nix
@@ -23,6 +23,8 @@
     pkgs.wakatime
     pkgs.firefox-wayland
 
+    pkgs.gh
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
