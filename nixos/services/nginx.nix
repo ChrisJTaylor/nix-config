7cf9ce41ced4a29b6dbd889105b0b9fbb316b48f
Christian Taylor <christianjtaylor@sky.com>
2024-12-01 13:55:38 +0000

    fix nginx websocket teamcity issues

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -6,20 +6,30 @@
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     virtualHosts = {
-      teamcity = {
-	serverName = "ci.machinology.local";
+      "ci.machinology.local" = {
 	serverAliases = [ "ci-static.machinology.local" ];
 	forceSSL = false;
 	enableACME = false;
 	locations = {
 	  "/" = {
 	    proxyPass = "http://127.0.0.1:8111/";
+            extraConfig = ''
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+              proxy_set_header Host $host;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+
+              # Optional: Increase timeout for WebSocket connections
+              proxy_read_timeout 3600s;
+              proxy_send_timeout 3600s;
+            '';
 	  };
 	};
       };
 
-      nevergreen = {
-	serverName = "builds.machinology.local";
+      "builds.machinology.local" = {
 	serverAliases = [ "builds-static.machinology.local" ];
 	forceSSL = false;
 	enableACME = false;
