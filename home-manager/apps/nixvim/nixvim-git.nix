276ba662619171cf360b656b6c881d80d705fb68
Christian Taylor <christianjtaylor@sky.com>
2025-01-03 13:16:33 +0000

    git blame current line

--- home-manager/apps/nixvim/nixvim-git.nix
+++ home-manager/apps/nixvim/nixvim-git.nix
@@ -23,7 +23,7 @@
       gitsigns = {
         enable = true;
         settings = {
-          current_line_blame = false;
+          current_line_blame = true;
           current_line_blame_opts = {
             virt_text = true;
             virt_text_pos = "eol";
