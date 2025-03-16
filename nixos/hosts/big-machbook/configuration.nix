a387728396e75756bd89125db3456ba4b7bb9361
Christian Taylor <christianjtaylor@sky.com>
2025-03-16 07:48:29 +0000

    use the right config setting

--- nixos/hosts/big-machbook/configuration.nix
+++ nixos/hosts/big-machbook/configuration.nix
@@ -26,6 +26,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
