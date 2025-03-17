bd73fdd4e3d05efa8a6c9519c7e02af347ff86a0
Christian Taylor <christianjtaylor@sky.com>
2025-03-17 09:55:02 +0000

    update gnome

--- nixos/system/gnome.nix
+++ nixos/system/gnome.nix
@@ -1,6 +1,31 @@
-{ ... }:
+{ config, ... }:
 
 {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.sudo.enableGnomeKeyring = true;
+
+  services.dbus.enable = true;
+  
+  services.xserver = {
+    enable = true;
+
+    videoDrivers = [
+      "nvidia"
+    ];
+
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = false;
+
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  hardware.nvidia.modesetting.enable = true;
 }
