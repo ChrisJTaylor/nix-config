69591b9738542109efd2dbf9e70f71b41bdc2420
Christian Taylor <christianjtaylor@sky.com>
2025-03-31 05:55:54 +0100

    start building dev env templates
    this is a work in progress

---
+++ home-manager/files/_dev_envs/golang/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 22; # Change this to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+            golangci-lint
+
+            watchexec
+          ];
+
+          shellHook = ''
+            just --list
+            '';
+        };
+      });
+    };
+}
