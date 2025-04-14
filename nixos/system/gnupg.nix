2488c37757eba0c3aa7b694b541af85ae2b662d5
Christian Taylor <christianjtaylor@sky.com>
2025-04-14 20:31:11 +0100

    enable pinentry

--- nixos/system/gnupg.nix
+++ nixos/system/gnupg.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     pkgs.gnupg
+    pkgs.pinentry-curses
   ];
 
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-curses;
   };
 }
