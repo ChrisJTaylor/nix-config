6a1ebcc37f9af9d613feb49d2e4e5162cfb7ec5f
Christian Taylor <christianjtaylor@sky.com>
2025-04-18 07:32:20 +0100

    add user password

--- nixos/users/christian.nix
+++ nixos/users/christian.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  # sops.secrets.user_password.neededForUsers = true;
+  sops.secrets.password_christian.neededForUsers = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.christian = {
@@ -11,7 +11,7 @@
     isNormalUser = true;
     description = "Christian Taylor";
     home = "/home/christian";
-    # hashedPasswordFile = config.sops.secrets.user_password.path;
+    hashedPasswordFile = config.sops.secrets.password_christian.path;
     shell = pkgs.zsh;
     extraGroups = ["networkmanager" "wheel" "docker" "plugdev" "podman"];
     packages = with pkgs; [
--- secrets/sops.nix
+++ secrets/sops.nix
@@ -1,11 +1,17 @@
 {...}: {
   sops = {
-    age.keyFile = "/root/.config/sops/age/keys.txt"; # or wherever your key is
+    age.keyFile = "/root/.config/sops/age/keys.txt";
+
+    defaultSopsFile = ./mysecret.yaml;
 
     secrets = {
       mysecret = {
         sopsFile = ./mysecret.yaml;
         path = "/etc/secrets/mysecret";
+      };
+
+      password_christian = {
+        sopsFile = ./mysecret.yaml;
       };
     };
   };
