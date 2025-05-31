b11badcf24999d769b21dfe82a9f7c8a6f0f6e34
Christian Taylor <christianjtaylor@sky.com>
2025-05-31 11:51:53 +0100

    add gh dash

--- home-manager/apps/gh.nix
+++ home-manager/apps/gh.nix
@@ -1,17 +1,19 @@
-{ ... }:
-
-{
+{...}: {
   programs.gh = {
     enable = true;
 
     gitCredentialHelper.enable = true;
 
     extensions = [
-
     ];
 
     settings = {
+    };
+  };
 
+  programs.gh-dash = {
+    enable = true;
+    settings = {
     };
   };
 }
