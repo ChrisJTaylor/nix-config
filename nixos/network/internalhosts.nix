dec33ea1f8485c273d068cff57f1eef9bebd543c
Christian Taylor <christianjtaylor@sky.com>
2025-11-02 19:04:56 +0000

    Add cache.machinology.local to internal hosts file
    Map cache.machinology.local to mach-serve-01's IP (127.0.0.2) for local network resolution

--- nixos/network/internalhosts.nix
+++ nixos/network/internalhosts.nix
@@ -1,6 +1,7 @@
 {...}: {
-  networking.extraHosts = ''
-    127.0.0.1    ci.machinology.local
-    127.0.0.1    builds.machinology.local
-  '';
+   networking.extraHosts = ''
+     127.0.0.1    ci.machinology.local
+     127.0.0.1    builds.machinology.local
+     127.0.0.2    cache.machinology.local
+   '';
 }
