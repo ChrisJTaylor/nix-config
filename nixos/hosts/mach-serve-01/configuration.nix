9e8bcd59edfe89cea9471dd39d77efa1e6d95296
Christian Taylor <christianjtaylor@sky.com>
2025-11-23 20:24:35 +0000

    Revert "fix: fix issue with harmonia config"
    This reverts commit 198897364ed789e837532f730d71ab49840135ef.

--- nixos/hosts/mach-serve-01/configuration.nix
+++ nixos/hosts/mach-serve-01/configuration.nix
@@ -41,16 +41,10 @@
     warningMinutes = 5;
   };
 
-  # Enable Harmonia daemon for proper upload support
-  services.harmonia-dev.daemon.enable = true;
-  
   services.harmonia-dev.cache.enable = true;
   # FIXME: generate a public/private key pair like this:
   # $ nix-store --generate-binary-cache-key cache.yourdomain.tld-1 /var/lib/secrets/harmonia.secret /var/lib/secrets/harmonia.pub
   services.harmonia-dev.cache.signKeyPaths = ["/var/lib/secrets/harmonia.secret"];
-  
-  # Ensure harmonia user can access Nix store
-  nix.settings.allowed-users = ["harmonia"];
 
   # Auto-generate Harmonia signing key pair if missing (idempotent)
   system.activationScripts.generateHarmoniaKey = lib.stringAfter ["etc"] ''
