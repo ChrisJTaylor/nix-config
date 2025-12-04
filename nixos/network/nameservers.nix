d2df28642ccb1cb8d84f44e62954ec46f57d843d
Christian Taylor <christianjtaylor@sky.com>
2025-12-04 05:38:04 +0000

    fix: add router to nameservers for ordering

--- nixos/network/nameservers.nix
+++ nixos/network/nameservers.nix
@@ -1,11 +1,8 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   # NixOS-only nameserver configuration (Linux systems only)
   networking.nameservers = [
     "192.168.1.246" # UGreen NAS
+    "192.168.1.254" # Router
     "1.1.1.1" # Cloudflare DNS
     "8.8.8.8" # Google DNS
   ];
