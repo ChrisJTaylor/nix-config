9758f6557a86b0d6c1c7ae7de7b4e3ffe5ac9917
Christian Taylor <christianjtaylor@sky.com>
2025-12-28 11:17:13 +0000

    fix: add cache.nixos.org trusted key

--- nixos/system/harmonia-cache-consumer.nix
+++ nixos/system/harmonia-cache-consumer.nix
@@ -1,8 +1,12 @@
 {...}: {
   nix.settings = {
-    substituters = ["http://cache.machinology.local" "https://cache.nixos.org"];
+    substituters = [
+      "http://cache.machinology.local"
+      "https://cache.nixos.org"
+    ];
     trusted-public-keys = [
       (builtins.readFile ../../none-secrets/binary-cache-public-key.pem)
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
