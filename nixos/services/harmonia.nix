aafb46349fdd1b99086eab5656f7db38aa95bed0
Christian Taylor <christianjtaylor@sky.com>
2025-12-28 18:55:47 +0000

    fix: route metrics endpoint through nginx

--- nixos/services/harmonia.nix
+++ nixos/services/harmonia.nix
@@ -32,6 +32,17 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:5000";
       };
+      # Add metrics endpoint proxy
+      locations."/metrics" = {
+        proxyPass = "http://127.0.0.1:5000/metrics";
+        extraConfig = ''
+          # Pass through original headers
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
     };
   };
 }
