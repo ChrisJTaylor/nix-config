e55ef09b0d8e92c97947f5266d51f0a590248dec
Christian Taylor <christianjtaylor@sky.com>
2025-12-28 10:47:22 +0000

    fix: public cert needs to be a relative path

--- nixos/system/harmonia-cache-consumer.nix
+++ nixos/system/harmonia-cache-consumer.nix
@@ -2,7 +2,7 @@
   nix.settings = {
     substituters = ["http://cache.machinology.local" "https://cache.nixos.org"];
     trusted-public-keys = [
-      (builtins.readFile ./none-secrets/binary-cache-public-key.pem)
+      (builtins.readFile ../../none-secrets/binary-cache-public-key.pem)
     ];
   };
 }
