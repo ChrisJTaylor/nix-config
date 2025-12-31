9cd35ce9e3d6c21d1602aafee52025762ee1146c
Christian Taylor <christianjtaylor@sky.com>
2025-12-31 16:56:02 +0000

    feat: run plasma in rootless mode

--- nixos/system/plasma.nix
+++ nixos/system/plasma.nix
@@ -6,6 +6,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.settings.General.DisplayServer = "wayland";
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
