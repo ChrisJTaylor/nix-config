2be88664c7b59e480235436db21ddb181b13f097
Christian Taylor <christianjtaylor@sky.com>
2026-01-02 13:59:40 +0000

    feat: reorder nameservers

--- nixos/network/nameservers.nix
+++ nixos/network/nameservers.nix
@@ -10,8 +10,8 @@
 
   # NixOS-only nameserver configuration (Linux systems only)
   networking.nameservers = [
-    "192.168.1.254" # Router
     "192.168.1.246" # UGreen NAS
+    "192.168.1.254" # Router
     "1.1.1.1" # Cloudflare DNS
     "8.8.8.8" # Google DNS
   ];
