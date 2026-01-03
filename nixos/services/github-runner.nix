658fbff0fedb9fa4fad66ee0d375a4ce97b336c5
Christian Taylor <christianjtaylor@sky.com>
2026-01-03 10:58:17 +0000

    build: extract github runner config into separate module

---
+++ nixos/services/github-runner.nix
@@ -0,0 +1,13 @@
+{ name }: { config, ... }: {
+  services.github-runners.${name} = {
+    enable = true;
+    url = "https://github.com/machinology";
+    tokenFile = config.sops.secrets.github-runner-token.path;
+    inherit name;
+    extraLabels = ["nix" "nixos"];
+    # Replace the runner if the token changes
+    replace = true;
+    # Run as a dedicated user
+    user = "github-runner";
+  };
+}
--- nixos/services/github-runners.nix
+++ nixos/services/github-runners.nix
@@ -1,4 +1,14 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    (import ./github-runner.nix {name = "mach-runner-1";})
+    (import ./github-runner.nix {name = "mach-runner-2";})
+    (import ./github-runner.nix {name = "mach-runner-3";})
+  ];
+
   users.users.github-runner = {
     isSystemUser = true;
     group = "github-runner";
@@ -12,55 +22,5 @@
 
   sops.secrets.github-runner-token = {
     sopsFile = ../../secrets/github-runners.yaml;
-  };
-
-  services.github-runners = {
-    mach-runner-1 = {
-      enable = true;
-      url = "https://github.com/machinology";
-      tokenFile = config.sops.secrets.github-runner-token.path;
-
-      name = "mach-runner-1";
-
-      extraLabels = ["nix" "nixos"];
-
-      # Replace the runner if the token changes
-      replace = true;
-
-      # Run as a dedicated user
-      user = "github-runner";
-    };
-
-    mach-runner-2 = {
-      enable = true;
-      url = "https://github.com/machinology";
-      tokenFile = config.sops.secrets.github-runner-token.path;
-
-      name = "mach-runner-2";
-
-      extraLabels = ["nix" "nixos"];
-
-      # Replace the runner if the token changes
-      replace = true;
-
-      # Run as a dedicated user
-      user = "github-runner";
-    };
-
-    mach-runner-3 = {
-      enable = true;
-      url = "https://github.com/machinology";
-      tokenFile = config.sops.secrets.github-runner-token.path;
-
-      name = "mach-runner-3";
-
-      extraLabels = ["nix" "nixos"];
-
-      # Replace the runner if the token changes
-      replace = true;
-
-      # Run as a dedicated user
-      user = "github-runner";
-    };
   };
 }
