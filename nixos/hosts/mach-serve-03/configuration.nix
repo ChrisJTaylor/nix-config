4449b9329832b7ad12651ac494b382bc5f766253
Christian Taylor <christianjtaylor@sky.com>
2026-02-08 07:06:32 +0000

    feat: enable mach-serve-03 to remote build

--- nixos/hosts/mach-serve-03/configuration.nix
+++ nixos/hosts/mach-serve-03/configuration.nix
@@ -40,4 +40,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
+
+  # Enable remote builds to mach-serve-01
+  nix.remoteBuilder.enable = true;
 }
