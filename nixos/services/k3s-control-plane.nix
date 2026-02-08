5432c85b206daa3c8e04318553221a135a1018c4
Christian Taylor <christianjtaylor@sky.com>
2026-02-08 10:15:06 +0000

    feat: add k3s token

--- nixos/services/k3s-control-plane.nix
+++ nixos/services/k3s-control-plane.nix
@@ -1,7 +1,12 @@
-{approved-packages, ...}: {
+{
+  approved-packages,
+  config,
+  ...
+}: {
   services.k3s = {
     enable = true;
     role = "server";
+    tokenFile = config.sops.secrets.k3s-token.path;
     extraFlags = toString [
       "--tls-san=192.168.1.119"
       # "--disable=servicelb" # builtin lb should be disabled if changing load balancers later
@@ -16,6 +21,10 @@
     allowedUDPPorts = [
       8472 # flannel vxlan
     ];
+  };
+
+  sops.secrets.k3s-token = {
+    sopsFile = ./secrets/mach-serve-02.yaml;
   };
 
   environment.systemPackages = with approved-packages; [
